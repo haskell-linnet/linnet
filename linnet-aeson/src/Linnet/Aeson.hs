db8348303b8b22072ce6478decf9172a722a5b34
sergeykolbasov <whoisliar@gmail.com>
2019-08-23 23:20:20 +0300

    Add haddock documentation and README to linnet-aeson

--- linnet-aeson/src/Linnet/Aeson.hs
+++ linnet-aeson/src/Linnet/Aeson.hs
@@ -2,12 +2,23 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeSynonymInstances  #-}
 
+{-|
+Module: Linnet Aeson
+Description: Aeson JSON support for Linnet
+Copyright: (c) Sergey Kolbasov, 2019
+License: Apache License 2.0
+
+This package adds support of JSON requests & responses in Linnet library
+using <http://hackage.haskell.org/package/aeson aeson>.
+
+See the detailed documentation on <http://linnet.io linnet.io>.
+
+-}
 module Linnet.Aeson where
 
 import           Control.Arrow         (left)
 import qualified Data.Aeson            as Aeson
 import qualified Data.ByteString.Char8 as C8
-import           Data.Function         ((&))
 import           Linnet.ContentTypes   (ApplicationJson)
 import           Linnet.Decode
 import           Linnet.Encode
